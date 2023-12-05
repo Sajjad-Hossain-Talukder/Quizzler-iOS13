dd70103363a3351340f6bb51903a22980ec52c08
Sajjad Hossain Talukder <63524824+Sajjad-Hossain-Talukder@users.noreply.github.com>
2023-12-05 18:09:52 +0600

    commit

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let quiz = [
+        ["Dhaka is the capital of Bangladesh", "True"],
+        ["Delhi is the capital of Thailand","False"] ,
+        ["Bangkok  is the capital of South Africa","False"]
+    ]
+    var qNumber = 0
+    
+    @IBOutlet weak var questionText: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        askQuestion()
     }
-
+    
+    
+    @IBAction func buttonAction(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let actualAnswer = quiz[qNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Wow.....Right")
+        } else {
+            print("Wrong!!!")
+        }
+        qNumber += 1
+        qNumber %= 3
+        askQuestion()
+    }
+    
+    func askQuestion(){
+        questionText.text = quiz[qNumber][0]
+    }
+    
 
 }
 
